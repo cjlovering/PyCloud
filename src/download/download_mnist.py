9c6188a6e239b548c6f33c1eddcd54f820297fa3
Charles Lovering <cjlovering@wpi.edu>
2018-04-25 00:28:55 -0400

    fix dl

--- src/download/download_mnist.py
+++ src/download/download_mnist.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision import datasets, transforms
 
-def download_mnist():
+def download_mnist(args):
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('data', train=True, download=True,
                     transform=transforms.Compose([
@@ -20,5 +20,44 @@ def download_mnist():
                     ])),
         batch_size=args.batch_size, shuffle=True, num_workers=1)
 
+def get_args():
+    parser = ArgumentParser(description='')
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--d_embed', type=int, default=100)
+    parser.add_argument('--d_proj', type=int, default=300)
+    parser.add_argument('--d_hidden', type=int, default=300)
+    parser.add_argument('--n_layers', type=int, default=1)
+    parser.add_argument('--log_every', type=int, default=50)
+    parser.add_argument('--lr', type=float, default=.001)
+    parser.add_argument('--dev_every', type=int, default=1000)
+    parser.add_argument('--save_every', type=int, default=1000)
+    parser.add_argument('--dp_ratio', type=int, default=0.2)
+    parser.add_argument('--no-bidirectional', action='store_false', dest='birnn')
+    parser.add_argument('--preserve-case', action='store_false', dest='lower')
+    parser.add_argument('--no-projection', action='store_false', dest='projection')
+    parser.add_argument('--train_embed', action='store_false', dest='fix_emb')
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--save_path', type=str, default='results')
+    parser.add_argument('--vector_cache', type=str, default=os.path.join(os.getcwd(), '.vector_cache/input_vectors.pt'))
+    parser.add_argument('--word_vectors', type=str, default='glove.6B.100d')
+    parser.add_argument('--resume_snapshot', type=str, default='')
+
+
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--capture-results', type=bool, default=False,
+                        help='times each process (default: True)')
+    parser.add_argument('--restarts', type=int, default=1)
+
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
-    download_mnist()
+    download_mnist(get_args())
